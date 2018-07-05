c798a2e3bd4e30a980535d7a996ed2bfdf04c56f
AakanshaDhawan <aakansha2809@gmail.com>
2018-07-05 16:31:50 +0530

    stemming
    changing different forms of verb to the first form

---
+++ nltk/stemming.py
@@ -0,0 +1,16 @@
+from nltk.stem import PorterStemmer
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+ps = PorterStemmer()
+
+example_words = ["python","pythoner","pythoning","pythoned","pythonly"]
+
+
+for w in example_words:
+    print(ps.stem(w))
+	
+#output:  python
+#		  python
+#		  python
+#		  python
+#		  pythonli
